cfae4d3473d4e0831c5900b622236c9246b991a7
kurtyan <kurtyan@foxmail.com>
2016-10-25 00:44:07 +0800

    - modified ncl script factory, added a 'convention over configuration' way to find NclScript implementation class.

--- datamanager-core/src/main/java/cn/edu/tsinghua/cess/task/service/impl/executor/NclScriptFactory.java
+++ datamanager-core/src/main/java/cn/edu/tsinghua/cess/task/service/impl/executor/NclScriptFactory.java
@@ -1,21 +1,54 @@
 package cn.edu.tsinghua.cess.task.service.impl.executor;
 
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Component;
-
 import cn.edu.tsinghua.cess.component.context.ApplicationContextHolder;
 import cn.edu.tsinghua.cess.datamanager.nclscript.NclScript;
+import com.google.common.base.Optional;
+import com.google.common.base.Supplier;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
 
 @Component
 public class NclScriptFactory {
 
 	private Logger logger = Logger.getLogger(NclScriptFactory.class);
 	
-	public NclScript getNclScript(String scriptId) {
-        NclScript script =  (NclScript) ApplicationContextHolder.getContext().getBean(scriptId);
-        logger.info("created ncl script=" + script.getClass().toString());
+	public NclScript getNclScript(final String scriptId) {
+		logger.info("begin process getNclScript, scripId=" + scriptId);
 
-        return script;
+        return this._getNclScript(scriptId).or(new Supplier<NclScript>() {
+			@Override
+			public NclScript get() {
+			    throw new RuntimeException("no script object was found for scriptId=" + scriptId);
+			}
+		});
+	}
+
+	private Optional<NclScript> _getNclScript(String scriptId) {
+		try {
+			return Optional.fromNullable(this.getByClassName(scriptId));
+		} catch (Exception e) {
+			logger.error("error getting by class name, msg=" + e.getMessage(), e);
+			return Optional.fromNullable(getWithinContext(scriptId));
+		}
+	}
+
+	private NclScript getWithinContext(String scriptId) {
+		NclScript script = (NclScript) ApplicationContextHolder.getContext().getBean(scriptId);
+		logger.info("created ncl script=" + script.getClass().toString());
+
+		return script;
+	}
+
+	private NclScript getByClassName(String scriptId) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+		String className = NclScript.class.getPackage().getName() + "." + scriptId;
+        Class clazz = Class.forName(className);
+
+		return (NclScript) clazz.newInstance();
+	}
+
+	public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+		Object inst = new NclScriptFactory().getNclScript("PolarNHEF");
+		System.out.println(inst);
 	}
 
 }
